250bb00e62b6177d0adaba5741998bcda82139cb
Josh Evans <josh944@gmail.com>
2013-09-22 22:23:39 -0700

    Fixed issue pulling from Last.fm

--- app/models/album.rb
+++ app/models/album.rb
@@ -22,8 +22,8 @@ class Album < ActiveRecord::Base
    return if json["error"]
 
    begin
-     self.summary = json["album"]["wiki"]["summary"] unless self.summary
      self.image = json["album"]["image"][3]["#text"] unless self.image
+     self.summary = json["album"]["wiki"]["summary"] unless self.summary
    rescue
    end
   end
@@ -39,7 +39,6 @@ class Album < ActiveRecord::Base
                          :api_key => LastFm.api_key,
                          :format => "json"}
      ).to_s
-
    response = HTTParty.get(address)
    JSON.parse(response.body)
   end
