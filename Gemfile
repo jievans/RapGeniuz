743137ef93b1999d43bae476a8122f87a39c8b6c
Josh Evans <josh944@gmail.com>
2013-09-29 13:04:54 -0700

    Deleted old gems

--- Gemfile
+++ Gemfile
@@ -5,15 +5,11 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'redcarpet'
-
 gem 'jquery-ui-rails'
 
 gem 'nokogiri'
 
 gem 'bcrypt-ruby'
-
-gem 'reverse_markdown'
 
 gem 'rabl'
 
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,8 +1,3 @@
-require 'redcarpet'
-require 'reverse_markdown'
-require 'redcarpet_renderers'
-
-
 module SongsHelper
   
   def strip_javascript(old_lyrics)
@@ -21,35 +16,4 @@ module SongsHelper
     strip_javascript(html_lyrics)
   end
   
-  def markdown(lyrics, options = {})
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdowner = Redcarpet::Markdown.new(renderer)
-    markdowner.render(lyrics).html_safe
-  end
-
-  def non_block_markdown(lyrics, options = {})
-    renderer = Redcarpet::Render::WithoutBlock.new(options)
-    markdowner = Redcarpet::Markdown.new(renderer)
-    markdowner.render(lyrics).html_safe
-  end
-
-  def reverse_markdown(lyrics)
-    plain_lyrics = ReverseMarkdown.parse_element(lyrics)
-
-    pattern = /(?<lyric>\[.+?\]\()(?<link>\/\d+)(?<endparen>\))/m
-
-    plain_lyrics.gsub!(pattern) do |match|
-      $~[:lyric] + $~[:link][1..-1] + $~[:endparen]
-    end
-
-    plain_lyrics
-  end
-
-  def make_anchors(edited_lyrics)
-    pattern = /(\[)(.+?)(\])(\()(\d+)(\))/m
-
-    edited_lyrics.gsub(pattern) do |match|
-      "<a href=\"/#{$2}\">#{$5}</a>"
-    end
-  end
 end
